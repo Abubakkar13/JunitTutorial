f08e52a6006f25b17faf420305b79f6e934c0024
Amit Nargund <nasasatyanasa@gmail.com>
2024-03-27 09:56:11 +0530

    Remove unwanted files and methods

--- src/main/java/com/example/JunitTutorial/Calculator.java
+++ src/main/java/com/example/JunitTutorial/Calculator.java
@@ -6,10 +6,6 @@ public class Calculator {
         return (long) a + b;
     }
 
-    public int subtract(int a, int b) {
-        return a - b;
-    }
-
     public int divide(int a, int b) {
         if (b == 0) {
             throw new IllegalArgumentException("Denominator cannot be 0");
